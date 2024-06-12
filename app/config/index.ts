3e53644cc6a62fe87866a590b5f08ffcc44ddd63
kyh <kyh_0625@naver.com>
2024-06-12 23:26:19 +0900

    deploy-3

--- app/config/index.ts
+++ app/config/index.ts
@@ -1,1 +1,1 @@
-export const backUrl = process.env.NODE_ENV === 'production' ? 'http://api.nodebird.com' : 'http://localhost:3065';
+export const backUrl = process.env.NODE_ENV === 'production' ? 'https://egoingboard.vercel.app/' : 'http://localhost:3065';
