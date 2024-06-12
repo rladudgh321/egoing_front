c54e0875b6086d938cac3abe09373d1ecd34546d
kyh <kyh_0625@naver.com>
2024-06-12 23:46:44 +0900

    deploy-4

--- app/config/index.ts
+++ app/config/index.ts
@@ -1,1 +1,1 @@
-export const backUrl = process.env.NODE_ENV === 'production' ? 'https://egoingboard.vercel.app/' : 'http://localhost:3065';
+export const backUrl = process.env.NODE_ENV === 'production' ? 'https://egoingboard-ba08w13os-rladudgh321s-projects.vercel.app/' : 'http://localhost:3065';
